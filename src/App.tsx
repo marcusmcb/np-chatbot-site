c84f12b7659d9c7da1f08568ca24bae2967f4e23
Marcus McBride <marcusmcb@gmail.com>
2023-07-18 16:03:02 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* main div */}
+      <div
+        style={{
+          marginLeft: "5vw",
+          marginRight: "5vw",
+          backgroundColor: "black",
+          height: "250px",
+        }}
+      >
+        {/* header div */}
+        <div
+          style={{
+            color: "yellow",
+            border: "2px solid white",
+            height: "50px",
+            paddingTop: "20px",
+            fontWeight: "600",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          npChatBot Site
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            color: 'white'
+          }}
+        >
+          <div>ONE</div>
+          <div>TWO</div>
+        </div>
+      </div>
     </div>
   );
 }
