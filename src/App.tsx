390745e4d9127b61ce8a5b1649f9fa53d2b71e0d
Marcus McBride <marcusmcb@gmail.com>
2023-07-19 11:57:53 -0700

    added basic landing page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,26 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* main div */}
-      <div
-        style={{
-          marginLeft: "5vw",
-          marginRight: "5vw",
-          backgroundColor: "black",
-          height: "250px",
-        }}
-      >
-        {/* header div */}
-        <div
-          style={{
-            color: "yellow",
-            border: "2px solid white",
-            height: "50px",
-            paddingTop: "20px",
-            fontWeight: "600",
-          }}
-        >
-          npChatBot Site
+      <div className="banner">
+        npChatbot For Twitch
+      </div>
+      <div className="description">
+        This is where the future description of the npChatbot app and everything that it's capable of doing.  It'll have way more text in here once it's complete and will fill out the element properly.
+      </div>
+      <div className="download-section">
+        <div className="download-box">
+          <p>Download npChatbot For Twitch</p>
+          <button>Download</button>
         </div>
-
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            color: 'white'
-          }}
-        >
-          <div>ONE</div>
-          <div>TWO</div>
+        <div className="description-box">
+          Here's a secondary element on the page that will display some relevant text to the user once it's been completed, tested, and deployed up to the live site for people to view and use and download.
         </div>
+      </div>
+      <div className="footer">
+        npChatbot, 2023
       </div>
     </div>
   );
