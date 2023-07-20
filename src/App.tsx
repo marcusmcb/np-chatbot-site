1b1c439b725d6b8d569612837e9a79b007b430a0
Marcus McBride <marcusmcb@gmail.com>
2023-07-19 17:26:20 -0700

    updated css for full responsive styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import './App.css'
 
-function App() {
+const App = () => {
 	return (
 		<div className='App'>
 			<div className='banner navbar'>
@@ -20,11 +19,9 @@ function App() {
 					</div>
 					<div className='description-box-text'>
 						<p>
-							npChatbot for Twitch allows you to integrate your Serato play
-							history directly into your Twitch channel. With a series of chat
-							commands enabled, your viewers can find out the currently playing
-							song in your stream, the previously played song, the longest song
-							played so far, and many others.
+							Use the npChatbot app to connect your Twitch chat with your Serato
+							Live Playlist and add a suite of interactive playlist commands to
+							your live-streamed DJ sets.
 						</p>
 					</div>
 				</div>
@@ -33,17 +30,54 @@ function App() {
 				<div className='details-inset'>
 					<div className='details-header'>Real Time Playlist Stats</div>
 					<p>
-						This is where the future description of the npChatbot app and
-						everything that it's capable of doing. It'll have way more text in
-						here once it's complete and will fill out the element properly.
+						npChatbot gives your viewers the ability to see your 'stats' in
+						real-time at any point during your stream.
 					</p>
+					<p>
+						For a quick check, they can simply enter the{' '}
+						<i>
+							<b>!stats</b>
+						</i>{' '}
+						command to see the total number of songs you've played so far, your
+						average song length for this set, and whether that average has
+						increased/decreased since the previous song.
+					</p>
+					<p>
+						For a deeper dive, viewers have access to commands that determine
+						the shortest and longest songs played so far in your set, as well as
+						how many times you've rocked doubles during your set and which songs
+						you were cutting it up with.
+					</p>
+					<p></p>
 				</div>
 				<div className='details-inset'>
-					<div className='details-header'>Open Up A World Of Music Discovery</div>
+					<div className='details-header'>
+						Open Up A World Of Music Discovery
+					</div>
 					<p>
-						This is where the future description of the npChatbot app and
-						everything that it's capable of doing. It'll have way more text in
-						here once it's complete and will fill out the element properly.
+						By opening up access to your real-time play history during your
+						live-stream DJ set, your viewers can query that history to see if
+						any specific artist or title has been played previously in your set.
+					</p>
+					<p>
+						By entering the{' '}
+						<i>
+							<b>!dyp</b>
+						</i>{' '}
+						command ("did you play...?") followed by an artist's name or song
+						title, your viewers can specifically see what you have (or haven't)
+						played yet during your stream.
+					</p>
+					<p>
+						The{' '}
+						<i>
+							<b>!np</b>
+						</i>{' '}
+						command ("now playing") and its options allow your viewers to see
+						the song currently playing, the previous song played (in case they
+						missed it!), the song you began your set with, and a "vibe check"
+						that returns a random selection from your play history
+						(title/artist) and how long ago you played it.
 					</p>
 				</div>
 			</div>
