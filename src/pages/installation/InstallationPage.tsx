ab60679b526b83c3f24d165172fd55344a0ce378
marcusmcb <marcusmcb@gmail.com>
2024-09-17 17:00:59 -0700

    fixed spacing errors, added images and text for installation

--- src/pages/installation/InstallationPage.tsx
+++ src/pages/installation/InstallationPage.tsx
@@ -6,31 +6,73 @@ const InstallationPage = (): JSX.Element => {
 			<div className='installation-page-header'>Installation</div>
 			<div className='installation-page-directions-container'>
 				<div className='installation-page-directions-step'>
-					1. Download the npChatbot app <a href='#downloadlink'>here</a>.
+					1. Download the npChatbot desktop app <a href='#downloadlink'>here</a>
+					.
 				</div>
+
 				<div className='installation-page-directions-step'>
-					2. Run the installer once the download has completed.
+					2. Double-click the installer file once the download has completed.
+					You'll see a loading image briefly before npChatbot fully loads.
 				</div>
+
 				<div className='installation-page-directions-step'>
-					3. Once installation has completed, start the npChatbot app.
+					3. Once loaded, click the{' '}
+					<span className='highlighted-installation-text'>Authorize</span>{' '}
+					button in the upper left to authorize npChatbot with Twitch
+					<span className='icon-span'>©</span>.
+					<img src='/images/installation/setup_01_final.png' />
 				</div>
+
 				<div className='installation-page-directions-step'>
-					3. Once started, click the "Authorize" button in the upper left to
-					authorize npChatbot with Twitch. You will need to be signed into the
-					account you want npChatbot to use for its responses (typically your
-					channel's chatbot account).
+					4. You should then see a prompt from Twitch
+					<span className='icon-span'>©</span> for you to authorize npChatbot.
+					Please be sure that you're signed into the Twitch
+					<span className='icon-span'>©</span> account that you would like
+					npChatbot to use for its responses (typically your channel's chatbot
+					account). Click{' '}
+					<span className='highlighted-installation-text'>Authorize</span> to
+					continue.
+					<img src='/images/installation/setup_02_final.png' />
 				</div>
+
 				<div className='installation-page-directions-step'>
-					4. Once authorized with Twitch, you should be returned to the
-					npChatbot app.
+					5. npChatbot will confirm the authorization and the app is now ready
+					for your credentials and preferences.
+					<img src='/images/installation/setup_03_final.png' />
 				</div>
+
 				<div className='installation-page-directions-step'>
-					4. Enter and save your credentials on first use. You will not have to
-					resubmit your credentials each time.
-				</div>
+					6. Enter in your primary Twitch<span className='icon-span'>©</span>{' '}
+					channel, your Twitch<span className='icon-span'>©</span> chatbot's
+					name, and your Serato<span className='icon-span'>©</span> profile
+					display name. Click{' '}
+					<span className='highlighted-installation-text'>Update</span> to save
+					your settings and credentials.
+					<img src='/images/installation/setup_05_final.png' />
+				</div>	
+
 				<div className='installation-page-directions-step'>
-					5. Set your OBS and interval message preferences.
+					8. Start npChatbot for your stream by clicking 'Connect'.
+					<img src='/images/installation/setup_10_final.png' />
+				</div>		
+
+				<div className='installation-page-directions-step'>
+					8. If you'd like to send npChatbot's responses to your OBS display,
+					enter in your OBS WebSocket address and password (if secured). Click{' '}
+					<span className='highlighted-installation-text'>Update</span> to save
+					your settings.
+					<img src='/images/installation/setup_08_final.png' />
 				</div>
+
+				<div className='installation-page-directions-step'>
+					9. Set your chatbot preferences for OBS display and interval messages.
+					Click <span className='highlighted-installation-text'>Update</span> to
+					save your settings.
+					<img src='/images/installation/setup_09_final.png' />
+				</div>
+
+				
+
 				<div className='installation-page-directions-step'>
 					6. Start a{' '}
 					<a
@@ -40,20 +82,29 @@ const InstallationPage = (): JSX.Element => {
 					>
 						Live Playlist
 					</a>{' '}
-					session in Serato DJ Pro.
+					session in Serato<span className='icon-span'>©</span> DJ Pro.
 				</div>
+
 				<div className='installation-page-directions-step'>
-					7. On the Serato Live Playlist page that pops up in your browser, edit
-					the playlist's settings and ensure that the playlist is set to
-					"public".
+					7. On the Serato<span className='icon-span'>©</span> Live Playlist
+					page that pops up in your browser, edit the playlist's settings and
+					ensure that the playlist is set to "public".
 				</div>
+
 				<div className='installation-page-directions-step'>
 					8. Start npChatbot for your stream by clicking 'Connect'.
 				</div>
+
 				<div className='installation-page-directions-step'>
 					9. Once you begin playing tracks in your stream, responses from the
-					npChatbot commands will be visible in your Twitch stream as chat text
-					or, optionally, on-screen in your OBS display.
+					npChatbot commands will be visible in your Twitch
+					<span className='icon-span'>©</span> stream as chat text or,
+					optionally, on-screen in your OBS display.
+				</div>
+
+				<div className='installation-page-directions-step'>
+					10. If you change the password for the account that npChatbot is
+					linked to, you'll need to reauthorize the app.
 				</div>
 			</div>
 		</div>
