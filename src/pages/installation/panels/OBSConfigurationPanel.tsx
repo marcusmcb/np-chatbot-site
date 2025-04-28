57ba3573e5b7be134181e66c868df40d2bc4db30
marcusmcb <marcusmcb@gmail.com>
2025-04-28 11:01:24 -0700

    updated OBS config instructions with additional MacOS and setup detail

--- src/pages/installation/panels/OBSConfigurationPanel.tsx
+++ src/pages/installation/panels/OBSConfigurationPanel.tsx
@@ -1,4 +1,4 @@
-import IconSpan from "../../components/IconSpan"
+import IconSpan from '../../components/IconSpan'
 
 const OBSConfigurationPanel = (): JSX.Element => {
 	return (
@@ -8,8 +8,9 @@ const OBSConfigurationPanel = (): JSX.Element => {
 			</div>
 			<div className='installation-page-directions-step'>
 				<p>
-					If you'd like to send npChatbot's responses to your OBS configuration,
-					enter in your OBS WebSocket address and password (if secured).
+					If you'd like to send npChatbot's responses to your OBS software to
+					display on-screen during your stream, enter in your OBS WebSocket
+					address and password (if secured).
 				</p>
 				<p>
 					Your OBS Websocket address needs to be given in the format shown in
@@ -19,7 +20,20 @@ const OBSConfigurationPanel = (): JSX.Element => {
 					Click <span className='highlighted-installation-text'>Update</span> to
 					save your settings.
 				</p>
-
+				<hr />
+				<p className='highlighted-installation-text'>Note for Mac users:</p>
+				<p>
+					When updating or entering your OBS websocket address and password for
+					the first time, you may see an error message from npChatbot noting
+					that it was unable to save your data along with a MacOS system prompt.
+				</p>
+				<p>
+					Select{' '}
+					<span className='highlighted-installation-text'>Allow Access</span>{' '}
+					from the prompt to allow npChatbot to connect to OBS and then press
+					"Update" again. You should see a message indicating that your
+					information was properly updated.
+				</p>
 				<img
 					alt='npChatbot instructions, step 9'
 					src='/images/installation/setup_08_final.png'
@@ -62,14 +76,44 @@ const OBSConfigurationPanel = (): JSX.Element => {
 					<span className='highlighted-installation-text'>
 						npchatbot-response
 					</span>{' '}
-					in any/all OBS scenes that you'd like the responses displayed in.
+					in any/all scenes that you'd like the responses displayed in. Disable
+					the visibility of this element in each scene you have it placed in.{' '}
+				</p>
+
+				<p>
+					With the OBS feature enabled, using the npChatbot commands will set
+					this element as visible on-screen in OBS. The{' '}
+					<span className='highlighted-installation-text'>
+						Display time (in seconds)
+					</span>{' '}
+					option in your preferences will determine how long the text is
+					displayed on screen before being cleared.
 				</p>
 				<p>
-					How you style and place the text data in your OBS overlays is up to
-					you. You can test placement in OBS by following the start up
-					instructions for npChatbot without actually starting a live stream on
-					Twitch<IconSpan/>. Update your preferences to set the OBS display time to a
-					longer duration to test placement within your OBS scenes or overlays.
+					<span className='highlighted-installation-text'>
+						Styling & Testing On-Screen Text Placement in OBS
+					</span>
+				</p>
+				<p>
+					How you style and place the text data within your OBS overlays is up
+					to you. The demo clips of npChatbot on the{' '}
+					<span className='highlighted-installation-text'>Features</span> page
+					are styled with a custom color and font imported into OBS with the
+					display time set to 5 seconds.
+				</p>
+				<p>
+					You can test placement in OBS by following the start up instructions
+					for npChatbot without actually starting a live stream on Twitch
+					<IconSpan />. Start a Live Playlist in Serato
+					<IconSpan /> and then start and connect npChatbot to Twitch with OBS
+					running.
+				</p>
+				<p>
+					You can then use npChatbot's commands in OBS' chat dock to test the
+					styling and placement of its responses in your OBS setup. Update your
+					preferences to set the OBS display time to a longer duration (60
+					seconds or more) to make it easier to test styling and placement while
+					you're not streaming.
 				</p>
 			</div>
 		</div>
